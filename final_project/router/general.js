c21cff8e51efddbd8e34ec73d94eecdc5984ec48
Bphan11 <123brucephan@gmail.com>
2023-04-14 00:28:36 -0400

    task 13 done
    Signed-off-by: Bphan11 <123brucephan@gmail.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,31 +24,40 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  let showBook = new Promise((resolve, reject) =>{
   res.send(JSON.stringify(books,null,4));
-
+  })
+  //if promise completes
+  showBook.then(() => console.log("Promise to display books is done(task 10)"));
  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  let showBookIsbn = new Promise((resolve, reject) =>{
+    const isbn = req.params.isbn;
+     res.send(books[isbn]);
+  })
+  showBookIsbn.then(() => console.log("Promise to display books via isbn is done(task 11)"));
  // return res.status(300).json({message: "Yet to be implemented1"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  let booksbyauthor = [];
-  let isbns = Object.keys(books);
-  isbns.forEach((isbn) => {
-    if(books[isbn]["author"] === req.params.author) {
-      booksbyauthor.push({"isbn":isbn,
-                          "author":books[isbn]["author"],
-                          "title":books[isbn]["title"],
-                          "reviews":books[isbn]["reviews"]});
-    }
+  let showBookAuthor = new Promise((resolve, reject) =>{
+     let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+        if(books[isbn]["author"] === req.params.author) {
+          booksbyauthor.push({"isbn":isbn,
+                             "author":books[isbn]["author"],
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+        }
+    })
+    showBookAuthor.then(() => console.log("Promise to display books via author is done(task 12)"));
   });
   res.send(JSON.stringify({booksbyauthor}, null, 4));
  // return res.status(300).json({message: "Yet to be implemented"});
@@ -57,15 +66,18 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  let booksbytitle = [];
-  let isbns = Object.keys(books);
-  isbns.forEach((isbn) => {
-    if(books[isbn]["title"] === req.params.title) {
-      booksbytitle.push({"isbn":isbn,
-                          "author":books[isbn]["author"],
-                          "title":books[isbn]["title"],
-                          "reviews":books[isbn]["reviews"]});
-    }
+  let showBookTitle = new Promise((resolve, reject) =>{
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+     isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+          booksbytitle.push({"isbn":isbn,
+                           "author":books[isbn]["author"],
+                           "title":books[isbn]["title"],
+                              "reviews":books[isbn]["reviews"]});
+      }
+    })
+    showBookTitle.then(() => console.log("Promise to display books via title is done(task 13)"));
     });
     res.send(JSON.stringify({booksbytitle}, null, 4));
  // return res.status(300).json({message: "Yet to be implemented"});
